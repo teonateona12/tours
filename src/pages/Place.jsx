3d07899ae9100e82f6ce2213861f9e86daa44b87
teonateona12 <teona.piranishvili1@gmail.com>
2024-02-18 13:58:57 +0400

    style: add Sighnaghi

--- src/pages/Place.jsx
+++ src/pages/Place.jsx
@@ -5,6 +5,11 @@ import BotanicGarden from "../assets/botanicGarden.jpg";
 import Mtawminda from "../assets/mtawminda.jpg";
 import OldTbilisi from "../assets/oldTbilisi.jpg";
 import Youtube from "../assets/youtube.png";
+import Sighnaghi from "../assets/sighnaghi.jpg";
+import Sighnaghi1 from "../assets/sighnaghi1.jpg";
+import Sighnaghi2 from "../assets/sighnaghi2.jpeg";
+import Sighnaghi3 from "../assets/sighnaghi3.jpg";
+
 const data = [
   {
     id: "tbilisi",
@@ -17,9 +22,14 @@ const data = [
     link: "https://l.facebook.com/l.php?u=https%3A%2F%2Fyoutu.be%2Ff3lV2SchOX8%3Fsi%3DmFYv9CA28uGMpR-K%26fbclid%3DIwAR09Bz7asBx0jUGEkfEMMPAfZ1Y4SlPAbdVC_koxMQZgkva9NFJ6m1qC0Kc&h=AT28VkwR2k2PfXakUBYeZHHOt9vMz0y7FUGJFZVZIQ5OXAU-zhN4fmMyVZaY9dHBJUT3wlUrA0o7cRPimwgyaVP9liEw_b6ML-3jQ7yHBZOxZ9MFFa44q7J-y0qgFQWWOfjx3Q",
   },
   {
-    id: "gudauri",
-    name: "Gudauri",
-    image1: Tbilisi,
+    id: "sighnaghi",
+    name: "Sighnaghi: The City of Love and Mesmerizing Views",
+    text: "Embark on a romantic journey in Sighnaghi, the charming City of Love nestled in the heart of Kakheti. As you wander through cobblestone streets, feel the essence of love that permeates the air. Marvel at the breathtaking views of the Alazani Valley and the Caucasus Mountains that surround this picturesque town.",
+    image1: Sighnaghi,
+    image2: Sighnaghi1,
+    image3: Sighnaghi2,
+    image4: Sighnaghi3,
+    link: "https://l.facebook.com/l.php?u=https%3A%2F%2Fyoutu.be%2FGvMyfgoucNo%3Fsi%3DCsgAfbWqwr3n0rZ2%26fbclid%3DIwAR2We_zD81zno0vK78WCfhRUE5kkzLJN9YBzFIXNBLH9REh4_IQEiUQ8hs8&h=AT28VkwR2k2PfXakUBYeZHHOt9vMz0y7FUGJFZVZIQ5OXAU-zhN4fmMyVZaY9dHBJUT3wlUrA0o7cRPimwgyaVP9liEw_b6ML-3jQ7yHBZOxZ9MFFa44q7J-y0qgFQWWOfjx3Q",
   },
 ];
 
